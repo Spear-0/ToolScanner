342345685b1713a4851f6d4ae7141cc4cf14738c
Spear-0 <2321708445@qq.com>
2022-04-08 22:08:51 +0800

    via

--- README.md
+++ README.md
@@ -1,45 +1,16 @@
 ### http yaml编写
 1、name(必须) 当前yaml名称
-
 2、protect(必须) 使用的协议（http）
-
 3、tool
-
     tool_name(必须) 检测工具的名称
-    
     tool_version(选填) 检测工具的版本
-
 4、request:
-
     method(必须):请求方式
-    
     path(选填):请求路径
-    
     data(选填):数据
-
 5、response: (一下两个选项选择必须要有一个以上不为空)
-
     pcre_body:匹配http body部分
-    
     pcre_header:匹配header格式为key: value 
-    
-    pcre_status:匹配响应首行
 
-### tcp yaml编写
 
-1、name
-
-2、ptotect
-
-3、tool
-
-    tool_name
-
-    tool_version
-4、request
-
-    data: 请求数据
-
-5、response
-
-    pcre_body: 响应匹配规则，默认接受2048字节的数据
+    ToolScanner v0.1
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"regexp"
+	"sync"
 
 	"gopkg.in/yaml.v2"
 )
@@ -15,7 +16,7 @@ import (
 var VERSION string = "v0.1"
 
 func banner() {
-	fmt.Printf("\tToolScanner %s\n", VERSION)
+	fmt.Printf("ToolScanner %s\n", VERSION)
 }
 
 type Yaml2Config struct {
@@ -33,21 +34,63 @@ type Yaml2Config struct {
 		Tool_name    string `yaml:"tool_name"`
 		Tool_version string `yaml:"tool_version"`
 	}
-	Name    string `yaml:"name"`
-	Protect string `yaml:"protect"`
+	Name     string `yaml:"name"`
+	Protocol string `yaml:"protocol"`
+}
+
+type ConfigQueue struct {
+	items []Yaml2Config
+	lock  sync.RWMutex
+}
+
+func (q *ConfigQueue) CreateQueue() *ConfigQueue {
+	q.items = []Yaml2Config{}
+	return q
+}
+
+func (q *ConfigQueue) Push(item Yaml2Config) {
+	q.lock.Lock()
+	q.items = append(q.items, item)
+	q.lock.Unlock()
+}
+
+func (q *ConfigQueue) Pop() *Yaml2Config {
+	q.lock.Lock()
+	item := q.items[len(q.items)-1]
+	q.items = q.items[:len(q.items)-1]
+	q.lock.Unlock()
+	return &item
+}
+
+func (q *ConfigQueue) Size() int {
+	return len(q.items)
+}
+
+func (q *ConfigQueue) IsEmpty() bool {
+	return q.Size() == 0
 }
 
 func main() {
+
 	banner()
+
 	var host = flag.String("s", "", "Target Address")
 	var port = flag.Int("p", 0, "Target Port")
 	flag.Parse()
 
+	log.Println("[*] Init config queue...")
+	var http ConfigQueue
+	var httpQueue = http.CreateQueue()
+	var tcp ConfigQueue
+	var tcpQueue = tcp.CreateQueue()
+
 	log.Printf("[*] target server: %s, target port: %d", *host, *port)
 	var target = fmt.Sprintf("%s:%d", *host, *port)
+
+	log.Println("[*] get yaml file")
 	yamls, err := ioutil.ReadDir("yaml/")
 	if err != nil {
-		log.Fatal("[-] get yaml fail")
+		log.Fatal("[-] get yaml file fail")
 	}
 
 	for _, fileInfo := range yamls {
@@ -55,7 +98,6 @@ func main() {
 			return
 		}
 		var filename = "yaml/" + fileInfo.Name()
-		//log.Printf("[*] Using %s", filename)
 		data, ioerr := ioutil.ReadFile(filename)
 		if ioerr != nil {
 			log.Fatal(ioerr)
@@ -65,27 +107,48 @@ func main() {
 		if err1 != nil {
 			log.Fatal(err1)
 		}
-		switch yamlconfig.Protect {
+		switch yamlconfig.Protocol {
 		case "http":
-			var url = fmt.Sprintf("http://%s", target)
-			if yamlconfig.Request.Path != "" {
-				url += yamlconfig.Request.Path
-			}
-			HTTPParse(yamlconfig, url)
+			httpQueue.Push(yamlconfig)
 		case "tcp":
-			conn, err := net.Dial("tcp", target)
-			if err != nil {
-				log.Fatalf("[-] %v", err)
-			}
-			defer conn.Close()
-			TCPaser(yamlconfig, conn)
+			tcpQueue.Push(yamlconfig)
 		default:
-			log.Fatalf("[-] unknown protect: %s", yamlconfig.Protect)
+			log.Printf("[-] Currently, the current protocol is not supported[%s]", yamlconfig.Name)
 		}
+
 	}
 
+	log.Println("[*] start connect target server...")
+	if !httpQueue.IsEmpty() {
+		ExecuteHTTPQueue(httpQueue, target)
+	}
+	if !tcpQueue.IsEmpty() {
+		ExecuteTCPQueue(tcpQueue, target)
+	}
+}
+func ExecuteHTTPQueue(q *ConfigQueue, target string) {
+	i := q.Size()
+	for {
+		item := q.Pop()
+		HTTPParse(*item, "http://"+target+item.Request.Path)
+		i -= 1
+		if i == 0 {
+			break
+		}
+	}
 }
 
+func ExecuteTCPQueue(q *ConfigQueue, target string) {
+	i := q.Size()
+	for {
+		item := q.Pop()
+		TCPPaser(*item, target)
+		i -= 1
+		if i == 0 {
+			break
+		}
+	}
+}
 func HTTPParse(yamlconfig Yaml2Config, url string) {
 	var isMatchBody = false
 	var isMatchStatus = false
@@ -93,12 +156,12 @@ func HTTPParse(yamlconfig Yaml2Config, url string) {
 	client := &http.Client{}
 	req, err := http.NewRequest(yamlconfig.Request.Method, url, nil)
 	if err != nil {
-		log.Printf("[-] create http client fail")
+		log.Printf("[-] %v", err)
 		return
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("[-] get response fail")
+		log.Printf("[-] %v", err)
 		return
 	}
 	body, err := ioutil.ReadAll(resp.Body)
@@ -149,11 +212,16 @@ func HTTPParse(yamlconfig Yaml2Config, url string) {
 		log.Printf("[%s %s]", yamlconfig.Tool.Tool_name, yamlconfig.Tool.Tool_version)
 	}
 }
-func TCPaser(yamlconfig Yaml2Config, conn net.Conn) {
+func TCPPaser(yamlconfig Yaml2Config, target string) {
+	conn, err := net.Dial("tcp", target)
+	if err != nil {
+		log.Fatalf("[-] %v", err)
+	}
+	defer conn.Close()
 	if yamlconfig.Request.Data != "" {
 		_, err := conn.Write([]byte(yamlconfig.Request.Data))
 		if err != nil {
-			log.Println("Write data fail")
+			log.Printf("[-] %v", err)
 			return
 		}
 	}
@@ -170,5 +238,4 @@ func TCPaser(yamlconfig Yaml2Config, conn net.Conn) {
 	}
 	return
 }
-
-
+func UDPPser(yamlconfig Yaml2Config) {}
